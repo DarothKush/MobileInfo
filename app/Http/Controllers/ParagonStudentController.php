e05e3dcded2b2946e00a66c27df7dd32d72c30d3
DarothKush <weeknessgt@gmail.com>
2024-02-05 16:54:28 +0700

    ini

---
+++ app/Http/Controllers/ParagonStudentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ParagonStudent;
+use Illuminate\Http\Request;
+
+class ParagonStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ParagonStudent $paragonStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ParagonStudent $paragonStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ParagonStudent $paragonStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ParagonStudent $paragonStudent)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProspectiveStudentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProspectiveStudent;
+use Illuminate\Http\Request;
+
+class ProspectiveStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProspectiveStudent $prospectiveStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProspectiveStudent $prospectiveStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProspectiveStudent $prospectiveStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProspectiveStudent $prospectiveStudent)
+    {
+        //
+    }
+}
