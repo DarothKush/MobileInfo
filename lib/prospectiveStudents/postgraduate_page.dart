d471287660f4d557666f873a8d5913b4287f28e3
PicheyUong <puong1@paragoniu.edu.kh>
2024-02-24 20:58:51 +0700

    chey

---
+++ lib/prospectiveStudents/postgraduate_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PostgraduatePage extends StatelessWidget {
+  const PostgraduatePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Postgraduate'),
+      ),
+      body: const Center(
+        child: Text('PostgraduatePage'),
+      ),
+    );
+  }
+}
---
+++ lib/prospectiveStudents/undergraduate_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UndergraduatePage extends StatelessWidget {
+  const UndergraduatePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Undergraduate'),
+      ),
+      body: const Center(
+        child: Text('UndergraduatePage'),
+      ),
+    );
+  }
+}
